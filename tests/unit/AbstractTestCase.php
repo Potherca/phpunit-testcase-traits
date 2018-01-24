813e5e534755ec0a0c292fe310a4fafc93f9c2bc
Ben Peachey <potherca@gmail.com>
2018-01-24 14:23:03 +0100

    Adds method to  AbstractTestCase to create a missing argument expectation.

--- tests/unit/AbstractTestCase.php
+++ tests/unit/AbstractTestCase.php
@@ -6,40 +6,37 @@ use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
-    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    //////////////////////////// SETTERS AND GETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    /**
-     * @param int $argumentCount
-     *
-     * @return array
-     */
-    final public function createMissingArgumentExpectation($argumentCount)
+    /** @return string[] */
+    private function getArgumentCountError()
     {
         $expectedExceptions = array(
             '5' => class_exists('\\PHPUnit_Framework_Error') ? '\\PHPUnit_Framework_Error' : '\\PHPUnit\\Framework\\Error',
             '7' => '\\ArgumentCountError',
             '70' => class_exists('\\PHPUnit_Framework_Error_Warning') ? '\\PHPUnit_Framework_Error_Warning' : '\\PHPUnit\\Framework\\Error\\Warning',
         );
-
-        $expectedExceptionMessages = array(
-            '5' => 'Missing argument ' . $argumentCount,
-            '7' => 'Too few arguments',
-            '70' => 'Missing argument ' . $argumentCount,
-        );
-
-        $function = $this->getExpectExpectationMethodName();
-
         $expectedException = $expectedExceptions[PHP_MAJOR_VERSION];
         if (array_key_exists(PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $expectedExceptions) === true) {
             $expectedException = $expectedExceptions[PHP_MAJOR_VERSION . PHP_MINOR_VERSION];
         }
 
-        $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION];
-        if (array_key_exists(PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $expectedExceptionMessages) === true) {
-            $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION . PHP_MINOR_VERSION];
+        return $expectedException;
+    }
+
+    /** @return string[] */
+    private function getArgumentTypeError()
+    {
+        $expectedExceptions = array(
+            '5' => class_exists('\\PHPUnit_Framework_Error') ? '\\PHPUnit_Framework_Error' : '\\PHPUnit\\Framework\\Error',
+            '7' => '\\TypeError',
+        );
+        $expectedException = $expectedExceptions[PHP_MAJOR_VERSION];
+        if (array_key_exists(PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $expectedExceptions) === true) {
+            $expectedException = $expectedExceptions[PHP_MAJOR_VERSION . PHP_MINOR_VERSION];
         }
 
-        return array($function, $expectedException, $expectedExceptionMessage);
+        return $expectedException;
     }
 
     /**
@@ -48,6 +45,7 @@ abstract class AbstractTestCase extends TestCase
     final public function getExpectExpectationMethodName()
     {
         $function = 'setExpectedException';
+
         if (method_exists($this, $function) === false) {
             $function = 'expectException';
         }
@@ -55,7 +53,95 @@ abstract class AbstractTestCase extends TestCase
         return $function;
     }
 
+    /**
+     * @return string
+     */
+    final public function getExpectExpectationRegexpMethodName()
+    {
+        $function = 'setExpectedExceptionRegExp';
+        if (method_exists($this, $function) === false) {
+            $function = 'expectExceptionMessageRegExp';
+        }
+
+        return $function;
+    }
+
+    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * @param int $argumentCount
+     *
+     * @return string[]
+     */
+    final public function createMissingArgumentExpectation($argumentCount)
+    {
+        $function = $this->getExpectExpectationMethodName();
+
+        $expectedException = $this->getArgumentCountError();
+
+        $expectedExceptionMessage = $this->getMissingArgumentMessage($argumentCount);
+
+        return array($function, $expectedException, $expectedExceptionMessage);
+    }
+
+    /**
+     * @param string $argumentType
+     *
+     * @return string[]
+     */
+    final public function createWrongArgumentTypeExpectation($argumentType)
+    {
+        $function = $this->getExpectExpectationMethodName();
+
+        $expectedException = $this->getArgumentTypeError();
+
+        $expectedExceptionMessage = $this->getWrongArgumentTypeMessage($argumentType);
+
+        return array($function, $expectedException, $expectedExceptionMessage);
+    }
+
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * @param int $argumentCount
+     *
+     * @return string[]
+     */
+    private function getMissingArgumentMessage($argumentCount)
+    {
+        $expectedExceptionMessages = array(
+            '5' => 'Missing argument ' . $argumentCount,
+            '7' => 'Too few arguments',
+            '70' => 'Missing argument ' . $argumentCount,
+        );
+        $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION];
+        if (array_key_exists(PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $expectedExceptionMessages) === true) {
+            $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION . PHP_MINOR_VERSION];
+        }
+
+        return $expectedExceptionMessage;
+    }
+
+    /**
+     * @param string $argumentType
+     *
+     * @return string[]
+     */
+    private function getWrongArgumentTypeMessage($argumentType)
+    {
+        $expectedExceptionMessages = array(
+            '5' => 'must be of the type ' . $argumentType,
+            '7' => 'must be of the type ' . $argumentType,
+            '53' => 'must be an ' . $argumentType,
+            '71' => 'Too few arguments',
+        );
+        $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION];
+        if (array_key_exists(PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $expectedExceptionMessages) === true) {
+            $expectedExceptionMessage = $expectedExceptionMessages[PHP_MAJOR_VERSION . PHP_MINOR_VERSION];
+        }
+
+        return $expectedExceptionMessage;
+    }
 }
 
 /*EOF*/
