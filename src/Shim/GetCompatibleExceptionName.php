99b59d56abb80d14f03a8a973287994e613863c5
Ben Peachey <potherca@gmail.com>
2018-01-25 14:28:30 +0100

    Fixes bug in GetCompatibleExceptionName caused by inconsistent behaviour in PHP70.

--- src/Shim/GetCompatibleExceptionName.php
+++ src/Shim/GetCompatibleExceptionName.php
@@ -12,9 +12,13 @@ use Potherca\PhpUnit\InvalidArgumentException;
 class GetCompatibleExceptionName extends AbstractTraitShim
 {
     //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    const ARGUMENT_COUNT_ERROR_WITH_TYPE_HINT = 'type-hint';
+    const ARGUMENT_COUNT_ERROR_WITHOUT_TYPE_HINT = 'no-type-hint';
+    const ERROR_CONTEXT_NEEDED = '%s needs a context to decide which exception name to provide. Available options are: "%s"';
 
     /**
      * @param string $exceptionName
+     * @param string $context
      *
      * @return string
      *
@@ -24,7 +28,7 @@ class GetCompatibleExceptionName extends AbstractTraitShim
      * @throws \PHPUnit_Framework_AssertionFailedError|\PHPUnit\Framework\AssertionFailedError
      * @throws \PHPUnit_Framework_SkippedTestError|\PHPUnit\Framework\SkippedTestError
      */
-    final public function getCompatibleExceptionName($exceptionName)
+    final public function getCompatibleExceptionName($exceptionName, $context = '')
     {
         $matchingExceptionName = '';
         $alternative = '';
@@ -48,23 +52,44 @@ class GetCompatibleExceptionName extends AbstractTraitShim
             if ($exceptionName === 'ParseError') {
                 $this->getTestcase()->markTestSkipped('Parse errors can not be caught in PHP5');
             } elseif ($exceptionName === 'ArithmeticError') {
-                /* PHP 7.0 thrown when an error occurs while performing
-                 * mathematical operations. As I have not been able to find the
-                 * PHP5 equivalent, marking as skipped until a working example
-                 * is available
+                /* PHP 7.0 thrown when an error occurs while performing mathematical operations.
+                 * As I have not been able to find the PHP5 equivalent, marking as skipped until
+                 * a working example is available.
                  */
                 $this->getTestcase()->markTestSkipped('There are no equivalent for Arithmetic errors in PHP5 ');
             } elseif ($exceptionName === 'ArgumentCountError') {
-                // PHP 7.1 thrown when too few arguments are passed to a user-defined function or method.
-                // PHP 7.0 throws a TypeError with message 'none given'
-                $matchingExceptionName = $this->getCompatibleExceptionName('\\TypeError');
+                /* PHP 7.1 thrown when too few arguments are passed to a user-defined function or method.
+                 * PHP 7.0 throws a TypeError if a type-hint is present or a warning if no
+                 * type-hint is present. The only way to know which one is needed is to ask the
+                 * calling side.
+                 */
+                $candidates = array(
+                    self::ARGUMENT_COUNT_ERROR_WITH_TYPE_HINT => $this->getCompatibleExceptionName('\\TypeError'),
+                    self::ARGUMENT_COUNT_ERROR_WITHOUT_TYPE_HINT => '\\PHPUnit_Framework_Error',
+                );
+
+                $candidate = self::ARGUMENT_COUNT_ERROR_WITHOUT_TYPE_HINT;
+
+                if (PHP_MAJOR_VERSION.PHP_MINOR_VERSION === '70') {
+                    if (array_key_exists((string) $context, $candidates) === false) {
+                        $exception = $this->getExistingClassName('\\PHPUnit\\Framework\\Exception');
+                        $error = vsprintf(self::ERROR_CONTEXT_NEEDED, array(
+                            'function' => __FUNCTION__,
+                            'candidates' => implode('", "', array_keys($candidates))
+                        ));
+                        throw new $exception($error);
+                    }
+                    $candidate = $context;
+                }
+                $matchingExceptionName = $candidates[$candidate];
             } else {
                 $matchingExceptionName = $this->getMatchingExceptionName($exceptionName);
 
-                if ($matchingExceptionName === '\\PHPUnit_Framework_Error') {
-                    $alternative = '\\PHPUnit\\Framework\\Error\\Error';
-                }
             }
+        }
+
+        if ($matchingExceptionName === '\\PHPUnit_Framework_Error') {
+            $alternative = '\\PHPUnit_Framework_Error_Error';
         }
 
         return $this->getExistingClassName($matchingExceptionName, $alternative);
